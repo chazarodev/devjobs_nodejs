eb73cb0348584ad22b24f47027dd0ec24aa74e7b
Alberto Cházaro <shazarerom@gmail.com>
2021-09-26 23:04:21 -0500

    Inicio de proyecto DevJobs

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const router= require('./routes/index');
+
+const app = express();
+
+//Habilitar handlebars como view
+app.engine('handlebars', 
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router());
+
+
+app.listen(5000);
