6b606db9a73068ef05e756a7e40c8a849d26f1b3
Alberto Cházaro <shazarerom@gmail.com>
2021-10-07 22:58:55 -0500

    Configurando nodemailer

---
+++ handlers/email.js
@@ -0,0 +1,14 @@
+const emailConfig = require('../config/email');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const util = require('util');
+
+let transport = nodemailer.createTransport({
+    host: emailConfig.host,
+    port: emailConfig.port,
+    auth: {
+        user: emailConfig.user,
+        pass: emailConfig.pass
+    }
+})
+
